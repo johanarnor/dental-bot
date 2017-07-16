2ca7f25da92fad65c9875c7ec70ba0db79f61bcd
Johan Arnör <johan.arnor@gmail.com>
2017-07-16 11:17:05 +0200

    Parsing of the page is somewhat done

--- handler.js
+++ handler.js
@@ -1,16 +1,39 @@
-'use strict';
+'use strict'
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const scrapeIt = require('scrape-it')
+// to avoid ssl errors (insecure I know...)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-  callback(null, response);
+module.exports.scrape = (event, context, callback) => {
+  scrapeIt('https://www.folktandvardenstockholm.se/webbokning/tillgangliga-tider/?lastmin=true', {
+    stuff: {
+      listItem: '.booking-main tbody tr',
+      data: {
+        date: '[data-heading=Datum]',
+        time: '[data-heading=Tid]',
+        clinic: '[data-heading=Klinik]',
+        treatment: '[data-heading=Behandling]',
+        link: {
+          selector: 'td a',
+          how: (value) => value[0].attribs.href
+        }
+      }
+    }
+  })
+  .then((page) => {
+    console.log(page)
+  })
+  .catch(console.error)
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event
+  //   })
+  // }
+
+  // callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+}
