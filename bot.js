7a5002989979fd25e88051a6d8eab3fb88bdae2f
Johan Arnör <johan.arnor@gmail.com>
2022-04-01 13:58:52 +0200

    Notify users about appointments

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 /* global fetch */
 import Prisma from '@prisma/client'
 import exampleData from './exampleData.js'
+import { sendEmail } from './email.js'
 
 process.env.TZ = 'Europe/Stockholm' // tell node to parse dates without timestamp (e.g. startTime) with Stockholm timezone
 
@@ -8,6 +9,26 @@ const prisma = new Prisma.PrismaClient()
 
 const alreadyExists = ({ id }) => prisma.appointment.findUnique({ where: { id } })
   .then(Boolean)
+
+const notifyUsers = async (appointment) => {
+  const users = await prisma.user.findMany({
+    where: {
+      active: true,
+      clinics: { has: appointment.clinic }
+    }
+  })
+  for (const user of users) {
+    await sendEmail({
+      to: user.email,
+      subject: `Ny tandläkartid i ${appointment.clinic}!`,
+      text: [
+        appointment.description,
+        `${appointment.startTime.toLocaleString('sv-SE')} i ${appointment.clinic}`,
+        'https://www.folktandvardenstockholm.se/webbokning/boka-sista-minuten/'
+      ].join('\n')
+    })
+  }
+}
 
 const main = async () => {
   // console.time('fetch')
@@ -30,7 +51,7 @@ const main = async () => {
       clinicName: clinic,
       startTime
     } = appointment
-    const saveResult = await prisma.appointment.create({
+    const savedAppointment = await prisma.appointment.create({
       data: {
         id,
         description,
@@ -38,7 +59,8 @@ const main = async () => {
         startTime: new Date(startTime)
       }
     })
-    console.log(saveResult)
+    console.log('saved appointment', JSON.stringify(savedAppointment))
+    await notifyUsers(savedAppointment)
   }
 }
 
